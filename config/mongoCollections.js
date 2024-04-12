5b0122de14af568c0c757d9183b908b70aad5ed5
Sumedh Badnore <66485793+sumedhbadnore@users.noreply.github.com>
2024-04-12 14:34:19 -0400

    Created basic project structure

---
+++ config/mongoCollections.js
@@ -0,0 +1,17 @@
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// Note: You will need to change the code below to have the collection required by the assignment!
+export const posts = getCollectionFn('products');
