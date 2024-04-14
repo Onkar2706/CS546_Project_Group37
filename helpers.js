cbd033197d70a0c3e8af988ba523a64e702147bb
Onkar <you@example.com>
2024-04-13 23:51:56 -0400

    password field updated

--- helpers.js
+++ helpers.js
@@ -1,18 +1,36 @@
+import bcrypt from "bcryptjs";
+
 const validate = {
+  async generatePassword() {
+    const saltRounds = 10;
+    const pw = "MyDummyPassword";
+    let hash = null;
+
+    try {
+      // Securing password
+      hash = await bcrypt.hash(pw, saltRounds);
+    } catch (e) {
+      console.log("unable to hash password");
+    }
+
+    return hash
+  },
 
   checkIfProperInput: (inp) => {
     if (!inp || inp === undefined) throw "Error: Input parameter not provided";
   },
 
   checkIfString: (inp) => {
-    if (typeof inp !== "string" || inp.trim().length === 0) throw "Error: Input parameter must be non-empty string";
+    if (typeof inp !== "string" || inp.trim().length === 0)
+      throw "Error: Input parameter must be non-empty string";
   },
 
   checkIfPositiveNumber: (inp) => {
-    if (typeof inp !== "number" || inp === NaN) throw "Error: Input parameter must be a positive number";
+    if (typeof inp !== "number" || inp === NaN)
+      throw "Error: Input parameter must be a positive number";
     if (inp < 1) throw "Error: Price must be positive";
     if (!Number.isInteger(inp)) {
-      let temp = inp.toString().split('.');
+      let temp = inp.toString().split(".");
       if (temp[1].length > 2) throw "Error: Only two decimal points allowed";
     }
   },
@@ -23,14 +41,17 @@ const validate = {
     const url = new URL(inp);
     if (strLength < 20) throw "Error: Invalid URL";
     if (url.protocol !== "http:") throw "Error: Invalid URL";
-    if (inp.substring(strLength - 4, strLength) !== ".com") throw "Error: Invalid URL";
+    if (inp.substring(strLength - 4, strLength) !== ".com")
+      throw "Error: Invalid URL";
     if (inp.slice(11, -4).length < 5) throw "Error: Invalid URL";
     let site = inp.slice(11, -4);
-    if (/^[a-zA-Z0-9]+([\-_\.][a-zA-Z0-9]+)*$/.test(site) === false) throw "Error: Invalid URL";   // https://javascript.plainenglish.io/check-if-string-is-alphanumeric-in-javascript-e325caa3ee
+    if (/^[a-zA-Z0-9]+([\-_\.][a-zA-Z0-9]+)*$/.test(site) === false)
+      throw "Error: Invalid URL"; // https://javascript.plainenglish.io/check-if-string-is-alphanumeric-in-javascript-e325caa3ee
   },
 
   checkIfValidArray: (inp) => {
-    if (typeof inp !== 'object' || Array.isArray(inp) !== true) throw "Error: Input parameter must be an array";
+    if (typeof inp !== "object" || Array.isArray(inp) !== true)
+      throw "Error: Input parameter must be an array";
     if (inp.length === 0) throw "Error: Empty array provided";
 
     for (let string of inp) {
@@ -42,25 +63,33 @@ const validate = {
     inp = inp.trim();
     let inpDate = new Date(inp);
     let todayDate = new Date();
-    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     if (inp.length !== 10) throw "Error: Invalid date";
-    if (parseInt(inp.substring(0, 2)) < 1 || parseInt(inp.substring(0, 2)) > 12) throw "Error: Invalid date"; //for checking month
+    if (parseInt(inp.substring(0, 2)) < 1 || parseInt(inp.substring(0, 2)) > 12)
+      throw "Error: Invalid date"; //for checking month
     let month = parseInt(inp.substring(0, 2));
-    if (parseInt(inp.substring(3, 5)) < 1 || parseInt(inp.substring(3, 5)) > days[month - 1]) throw "Error: Invalid date"; //for checking day
-    if (parseInt(inp.substring(6)) > todayDate.getFullYear()) throw "Error: Invalid date"; //for checking year
+    if (
+      parseInt(inp.substring(3, 5)) < 1 ||
+      parseInt(inp.substring(3, 5)) > days[month - 1]
+    )
+      throw "Error: Invalid date"; //for checking day
+    if (parseInt(inp.substring(6)) > todayDate.getFullYear())
+      throw "Error: Invalid date"; //for checking year
     if (inpDate > todayDate) throw "Error: Invalid Date"; // for checking if date in future
   },
 
   checkIfBoolean: (inp) => {
-    if (typeof inp !== 'boolean') throw "Error: Input parameter must be boolean";
+    if (typeof inp !== "boolean")
+      throw "Error: Input parameter must be boolean";
   },
 
   checkIfValidRating: (inp) => {
-    if (typeof inp !== 'number' || inp === NaN) throw "Error: Invalid Rating";
+    if (typeof inp !== "number" || inp === NaN) throw "Error: Invalid Rating";
     if (inp < 1 || inp > 5) throw "Error: Invalid Rating";
     if (!Number.isInteger(inp)) {
-      let temp = inp.toString().split('.');
-      if (temp[1].length > 1) throw "Error: Only one decimal points allowed for rating";
+      let temp = inp.toString().split(".");
+      if (temp[1].length > 1)
+        throw "Error: Only one decimal points allowed for rating";
     }
   },
 
@@ -75,7 +104,7 @@ const validate = {
       day = "0" + day;
     }
     let year = temp.getFullYear();
-    return `${month}/${day}/${year}`
+    return `${month}/${day}/${year}`;
   },
 
   calculateAverageRating: (arr) => {
@@ -85,31 +114,75 @@ const validate = {
     for (let i = 0; i < arr.length; i++) {
       sum += arr[i]["rating"];
     }
-    let result = (sum / arr.length);
+    let result = sum / arr.length;
     result = result.toFixed(1);
     result = parseFloat(result);
-    return result
+    return result;
   },
 
   validateState: (state) => {
     const States = [
-      "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
-      "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-      "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-      "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-      "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
-  ];
+      "AL",
+      "AK",
+      "AZ",
+      "AR",
+      "CA",
+      "CO",
+      "CT",
+      "DE",
+      "FL",
+      "GA",
+      "HI",
+      "ID",
+      "IL",
+      "IN",
+      "IA",
+      "KS",
+      "KY",
+      "LA",
+      "ME",
+      "MD",
+      "MA",
+      "MI",
+      "MN",
+      "MS",
+      "MO",
+      "MT",
+      "NE",
+      "NV",
+      "NH",
+      "NJ",
+      "NM",
+      "NY",
+      "NC",
+      "ND",
+      "OH",
+      "OK",
+      "OR",
+      "PA",
+      "RI",
+      "SC",
+      "SD",
+      "TN",
+      "TX",
+      "UT",
+      "VT",
+      "VA",
+      "WA",
+      "WV",
+      "WI",
+      "WY",
+    ];
     if (state === undefined) throw "Please enter valid state";
     if (typeof state !== "string") throw "Please give valid string";
     if (state.length !== 2) throw "Please enter only State shortforms";
     if (state.trim === "") throw "do not enter blank space";
     if (!States.includes(state.toUpperCase())) throw "State is not valid";
-    return true
-  }
+    return true;
+  },
 };
 
 export default validate;
-
 
 // // You can add and export any helper functions you want here - if you aren't using any, then you can just leave this file as is
 
