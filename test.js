992460a989f327ed975b459adc90fb2ddf269ddc
Onkar <you@example.com>
2024-04-14 22:52:11 -0400

    update function in user created

--- test.js
+++ test.js
@@ -2,9 +2,12 @@ import bcrypt from "bcryptjs";
 import validate from "./helpers.js";
 import user from "./data/users.js";
 
+let oId = null
+
 try {
-  console.log(
-    await user.create(
+    // oId= this._id
+  
+   let usercreated = await user.create(
       "Chandler",
       "Bing",
       "Monica27",
@@ -16,8 +19,42 @@ try {
       ["Purchase1", "Purchase2", "Purchase3", "Purchase4"],
       ["Post1", "Post2", "Post3"],
       "abc1266665b"
+      
     )
-  );    
+
+    console.log(usercreated)
+    
+    
+    
+  oId= usercreated._id
+  console.log(oId)
+} 
+catch (error) {
+    console.log(error)
+}
+
+
+
+try {
+  
+  console.log(
+    await user.updateUserInfo( oId,
+      "Chandler",
+      "Geller",
+      "Monica27",
+      "MyDummyPassword",
+      "RS27@email.com",
+      "NJ",
+      "Hoboken",
+      ["Book", "Arts", "Cars"],
+      ["Purchase1", "Purchase2", "Purchase3", "Purchase4"],
+      ["Post1", "Post2", "Post3"],
+      "abc1266665b"
+      
+    )
+    
+  );   
+  
 } 
 catch (error) {
     console.log(error)
