4d78ad6286c5d1c5d38dca51d992a607b771ca59
Sumedh Badnore <66485793+sumedhbadnore@users.noreply.github.com>
2024-04-16 20:12:26 -0400

    created seperate folder for home, login and register page

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ router
   .route('/')
   .get(async (req, res) => {
     try {
-        res.render("home.handlebars", {title: "Home Page"})
+        res.render("home/home.handlebars", {title: "Home Page"})
     }
     catch (e) {
       return res.status(400).json(e);
