b44af8b5630cc9e2d0a726081316b329f39175bd
Sumedh Badnore <66485793+sumedhbadnore@users.noreply.github.com>
2024-04-16 20:09:00 -0400

    created route for loading home page

---
+++ routes/home.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+router
+  .route('/')
+  .get(async (req, res) => {
+    try {
+        res.render("home.handlebars", {title: "Home Page"})
+    }
+    catch (e) {
+      return res.status(400).json(e);
+    }
+  });
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 import userRoutes from './user.js';
 import artistRoutes from './artists.js';
+import homeRoutes from "./home.js";
+import path from "path";
 
 
 const constructor = (app) => {
+    app.use('/', homeRoutes);
     app.use('/user', userRoutes);
     app.use('/artist', artistRoutes);
 
-
     app.use('*', (req, res) => {
-        res.status(404).json({error: 'Route Not Found'});
+        res.status(404).json("Error: Page not found");
     });
 };
 
