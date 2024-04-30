7d31941f4ed4749e97abff71d77703d12d9c235b
Onkar <you@example.com>
2024-04-29 21:43:59 -0400

    added logout functionality

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import userRoutes from "./user.js";
 import artistRoutes from "./artists.js";
 import homeRoutes from "./home.js";
+import logout from "./logout.js"
 import path from "path";
 
 const constructor = (app) => {
   app.use("/", homeRoutes);
   app.use("/user", userRoutes);
   app.use("/artist", artistRoutes);
+  app.use("/logout", logout);
 
   app.use("*", (req, res) => {
     res.status(404).render("error", { message: "Page Not Found" });
