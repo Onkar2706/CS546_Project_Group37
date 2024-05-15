94fd92efb0f3ef0a726d521a260f7c67ed790dcf
Onkar <you@example.com>
2024-05-15 00:12:29 -0400

    added a remote mongodb url

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,5 @@
 // You will need to change the DB name to match the required DB name in the assignment specs!
 export const mongoConfig = {
-  serverUrl: 'mongodb://localhost:27017/',
+  serverUrl: 'mongodb+srv://onkarmahamuni007:iGTsw9u5LpMn8loD@cluster0.3x9tqw2.mongodb.net/',
   database: 'Artisthub'
 };
